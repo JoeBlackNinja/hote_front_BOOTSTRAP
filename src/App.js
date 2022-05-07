293d0da15ed2d7339b178faa0c89e9d88bdb2d36
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-05-06 20:02:43 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from '../src/images/backpackers-logos_white.png';
+
+import { 
+  Button, 
+  Col,
+  Container,
+  Navbar,
+  Nav,
+  Row
+} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (    
+      <Container className='mainContainer' fluid>
+        <Row className='Header'>      
+          <Col xs={12}>
+            <Navbar className='rowNavBar'>
+            <Navbar.Brand className='Nav.Link.margin'>
+                <img src={logo} className='textLog'/>
+            </Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="/home">
+                  <label className='textNavBar'>Home</label>
+                </Nav.Link>
+                <Nav.Link href="#features">
+                  <label className='textNavBar'>About Us</label>
+                </Nav.Link>
+                <Nav.Link href="#pricing">
+                  <label className='textNavBar'>Reservation</label>
+                </Nav.Link>
+                <Nav.Link href="#pricing">
+                  <label className='textNavBar'>Bills</label>
+                </Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link 
+                    href=''
+                  ><Button 
+                    variant='outline-light'
+                    size='lg'
+                  >LOG IN</Button>
+                </Nav.Link>
+              </Nav>
+          </Navbar>            
+          </Col> 
+
+          <Col xs={12} className='bodyStyle'>
+            <h1>asdasd</h1>
+          </Col>
+
+        </Row>
+
+        
+          
+        
+      </Container>
+  
   );
 }
 
